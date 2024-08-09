564014ad2574e3c9eebcb998deed74d3e9c754d1
theluposki <theluposki@gmail.com>
2024-08-09 05:53:58 -0300

    add summary

--- src/views/cars/AddCar.vue
+++ src/views/cars/AddCar.vue
@@ -66,18 +66,10 @@ const send = () => {
   <main class="page int">
     <div class="form-control">
       <label for="placa" class="label-required">Placa:</label>
-      <input 
-      type="text" 
-      tabindex="1" 
-      :class="{'input': true, 'invalid': !placaValida}" 
-      v-model="vehicle.placa" 
-      id="placa"
-      maxlength="7"
-      @keyup="timeoutValidaPlaca"
-      placeholder="placa do veículo. EX: ABC1D123 ou ABC-1234"
-      @blur="validarPlaca"
-    >
-    <p v-if="!placaValida" class="error-message">Placa inválida!</p>
+      <input type="text" tabindex="1" :class="{ 'input': true, 'invalid': !placaValida }" v-model="vehicle.placa"
+        id="placa" maxlength="7" @keyup="timeoutValidaPlaca" placeholder="placa do veículo. EX: ABC1D123 ou ABC-1234"
+        @blur="validarPlaca">
+      <p v-if="!placaValida" class="error-message">Placa inválida!</p>
     </div>
 
     <div class="form-control">
@@ -93,44 +85,48 @@ const send = () => {
     <div class="form-group">
       <fieldset>
         <legend>Segmentação</legend>
-      <div class="field">
-        <label for="consumidor" class="label-radio consumidor">Consumidor</label>
-        <input type="radio" tabindex="3" id="consumidor" name="segmentacao" class="input-radio"
-          v-model="vehicle.segmentacao" value="consumidor">
-      </div>
-      <div class="field">
-        <label for="revenda" class="label-radio revenda">Revenda</label>
-        <input type="radio" tabindex="4" id="revenda" name="segmentacao" class="input-radio"
-          v-model="vehicle.segmentacao" value="revenda">
-      </div>
+        <div class="field">
+          <label for="consumidor" class="label-radio consumidor">Consumidor</label>
+          <input type="radio" tabindex="3" id="consumidor" name="segmentacao" class="input-radio"
+            v-model="vehicle.segmentacao" value="consumidor">
+        </div>
+        <div class="field">
+          <label for="revenda" class="label-radio revenda">Revenda</label>
+          <input type="radio" tabindex="4" id="revenda" name="segmentacao" class="input-radio"
+            v-model="vehicle.segmentacao" value="revenda">
+        </div>
       </fieldset>
     </div>
 
     <div class="form-group">
       <fieldset>
         <legend>Tipo de serviço</legend>
-      <div class="field">
-        <label for="tp" class="label-radio tp">Troca de peça</label>
-        <input type="radio" tabindex="5" id="tp" name="tipo" class="input-radio" v-model="vehicle.tipo" value="TP">
-      </div>
-      <div class="field">
-        <label for="sm" class="label-radio sm">Serviço mecânico</label>
-        <input type="radio" tabindex="6" id="sm" name="tipo" class="input-radio" v-model="vehicle.tipo"
-          value="SM">
-      </div>
+        <div class="field">
+          <label for="tp" class="label-radio tp">Troca de peça</label>
+          <input type="radio" tabindex="5" id="tp" name="tipo" class="input-radio" v-model="vehicle.tipo" value="TP">
+        </div>
+        <div class="field">
+          <label for="sm" class="label-radio sm">Serviço mecânico</label>
+          <input type="radio" tabindex="6" id="sm" name="tipo" class="input-radio" v-model="vehicle.tipo" value="SM">
+        </div>
       </fieldset>
     </div>
 
     <div class="form-control">
       <label for="qtdPecas" class="label-required">Quantidade de Peças:</label>
-      <input type="tel" min="1" max="2" maxlength="2" tabindex="7" class="input" v-model.number="vehicle.qtdPecas" id="qtdPecas" placeholder="Ex: 3">
+      <input type="tel" min="1" max="2" maxlength="2" tabindex="7" class="input" v-model.number="vehicle.qtdPecas"
+        id="qtdPecas" placeholder="Ex: 3">
     </div>
 
-    <div class="form-control">
-      <label for="observacao" class="label">Observação:</label>
-      <textarea class="textarea" v-model="vehicle.observacao" id="observacao"
-        placeholder="Alguma observação sobre o veículo."></textarea>
-    </div>
+
+    <details>
+      <summary>Alguma observação sobre o veículo?</summary>
+      <div class="form-control">
+        <label for="observacao" class="label">Observação:</label>
+        <textarea class="textarea" v-model="vehicle.observacao" id="observacao"
+          placeholder="Ex: veículo com amassado na porta d/d."></textarea>
+      </div>
+    </details>
 
     <div class="form-control">
       <button class="btn" @click="send" tabindex="8">
@@ -151,5 +147,4 @@ const send = () => {
   color: red;
   margin-top: 5px;
 }
-
 </style>
