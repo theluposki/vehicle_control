cc79633cdbc5bcddcf92b91ce6a2977a0609e655
theluposki <theluposki@gmail.com>
2024-08-21 06:24:45 -0300

    Updates

--- assets/ListCars-bZNEjajq.js
+++
@@ -1,1 +0,0 @@
-import{E as t}from"./Emitter-BG7KJySb.js";import{i as s,c as a,o as e}from"./index-D5_ZTehc.js";const r={class:"page int"},_={__name:"ListCars",setup(o){return s(()=>{t.emit("list-car")}),(c,i)=>(e(),a("main",r," Listar Carros "))}};export{_ as default};
