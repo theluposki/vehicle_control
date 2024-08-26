4d2114b9ccd4cbd280d6a3d50a15533326bb7e66
theluposki <theluposki@gmail.com>
2024-08-26 03:57:49 -0300

    list init

--- src/views/cars/ListCars.vue
+++ src/views/cars/ListCars.vue
@@ -3,6 +3,7 @@ import { db } from '@/db/databaseLocal';
 import { Emitter } from '@/utils/Emitter';
 import { onMounted, ref } from 'vue';
 import dayjs from 'dayjs';
+
 onMounted(async () => {
   Emitter.emit('list-car');
   list.value = await getListAll();
@@ -20,14 +21,133 @@ const getListAll = async () => {
   return await db.vehicle.toArray();
 }
 
+const defineClassConsumidor = (segmentacao) => {
+  console.log(segmentacao)
+  if(segmentacao === 'consumidor') {
+    return 'segmentacao consumidor'
+  }
+  return 'segmentacao revenda'
+}
+
 </script>
 
 <template>
   <main class="page int">
-    <ul class="list">
+    <ul class="list-cars">
       <li class="item" v-for="item in list" :key="item.id">
-        {{ item.modelo }} - {{ item.placa }} - {{ item.segmentacao }} - {{ formatDate(item.date) }}
+        <div class="left">
+          <span :class="defineClassConsumidor(item.segmentacao)">
+            {{ item.segmentacao }} 
+          </span>
+          <span class="placa">
+            {{ item.placa }} 
+          </span>
+          <span class="modelo">
+            {{ item.modelo }}
+          </span>
+        </div>
+        
+        <div class="right">
+          <span class="date">
+            {{ formatDate(item.date) }}
+          </span>
+        </div>
       </li>
     </ul>
   </main>
 </template>
+
+<style scoped>
+.list-cars {
+  list-style: none;
+  width: 100%;
+  min-height: var(--list-vehicle);
+  max-height: var(--list-vehicle);
+  overflow-x: hidden;
+  overflow-y: auto;
+
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  padding: 0;
+}
+
+.item {
+  --h_item: 70px;
+  min-width: 100%;
+  min-height: var(--h_item);
+  max-height: var(--h_item);
+  background-color: var(--white3);
+  padding: 0 var(--p-base);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+}
+
+.item:active {
+ scale: 1.015;
+}
+
+.item .left, .item .right {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  position: relative;
+  min-height: var(--h_item);
+}
+
+.item .left {
+  justify-content: flex-end;
+}
+
+.item .left .segmentacao {
+  position: absolute;
+  top: 2px;
+  left: -12px;
+  background-color: var(--silver3);
+  border-radius: 0 20px 20px 0;
+  padding: 0 var(--p-base);
+  font-size: 12px;
+  color: var(--white);
+  font-weight: bold;
+  transition: background-color ease .4s;
+}
+
+
+.item .left .placa {
+  color: var(--green3);
+  font-weight: 900;
+  font-size: 18px;
+  line-height: normal;
+}
+
+.item .left .modelo {
+  line-height: normal;
+  font-weight: bold;
+  color: var(--silver3);
+  text-shadow: 0 0 1px var(--green3);
+  padding-bottom: 6px;
+}
+
+.item .left .segmentacao.revenda {
+  background-color: var(--orange);
+}
+
+.item .left .segmentacao.consumidor {
+  background-color: var(--green2);
+}
+
+.item .right .date {
+  position: absolute;
+  top: 2px;
+  right: -12px;
+  background-color: var(--dark3);
+  font-size: 10px;
+  color: var(--white);
+  padding: 0 var(--p-base);
+  display: flex;
+  width: max-content;
+  border-radius: 20px 0 0 20px;
+}
+</style>
