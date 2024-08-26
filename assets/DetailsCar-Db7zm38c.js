ed3b836cd62684f7370bda088c4c2686d30c6371
theluposki <theluposki@gmail.com>
2024-08-26 00:33:36 -0300

    Updates

--- assets/DetailsCar-Db7zm38c.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./index-BdliIbcx.js";const c={},o={class:"page int"};function r(s,n){return a(),t("main",o," Detalhe do Carro ")}const i=e(c,[["render",r]]);export{i as default};
