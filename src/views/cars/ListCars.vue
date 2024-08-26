93890ccd97271aa8850a7d800dab48c6c3bb77a2
theluposki <theluposki@gmail.com>
2024-08-26 00:32:49 -0300

    dateFormat

--- src/views/cars/ListCars.vue
+++ src/views/cars/ListCars.vue
@@ -2,13 +2,19 @@
 import { db } from '@/db/databaseLocal';
 import { Emitter } from '@/utils/Emitter';
 import { onMounted, ref } from 'vue';
-
+import dayjs from 'dayjs';
 onMounted(async () => {
   Emitter.emit('list-car');
   list.value = await getListAll();
 })
 
 const list = ref([]);
+
+const formatDate = (isoString) => {
+  const date = dayjs(isoString);
+  const formattedDate = date.format('DD/MM/YYYY HH:mm:ss');
+  return formattedDate
+}
 
 const getListAll = async () => {
   return await db.vehicle.toArray();
@@ -17,12 +23,11 @@ const getListAll = async () => {
 </script>
 
 <template>
-    <main class="page int">
-      <ul class="list">
-        <li class="item" v-for="item in list" :key="item.id">
-          {{ item.placa }}
-        </li>
-      </ul>
-    </main>
-  </template>
-  
+  <main class="page int">
+    <ul class="list">
+      <li class="item" v-for="item in list" :key="item.id">
+        {{ item.modelo }} - {{ item.placa }} - {{ item.segmentacao }} - {{ formatDate(item.date) }}
+      </li>
+    </ul>
+  </main>
+</template>
