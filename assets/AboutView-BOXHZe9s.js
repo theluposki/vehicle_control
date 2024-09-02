f27d4d6c79ba48e480871db3be9f3ba2591ad224
theluposki <theluposki@gmail.com>
2024-09-02 08:25:59 -0300

    Updates

--- assets/AboutView-BOXHZe9s.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-C3LY4VYU.js";const t={},s={class:"page"};function n(a,r){return o(),c("main",s," About ")}const f=e(t,[["render",n]]);export{f as default};
---
+++ assets/user-IH9lAbMQ.js
@@ -0,0 +1,1 @@
+import{y as r,r as o}from"./index-oI48y2kd.js";const u=["Argo","Onix","Onix Plus","KA","Mobi"],c=r("user",()=>{const e=o({id:window.crypto.randomUUID(),nome:"Lucas Oliveira",matricula:420033,setor:"Troca de Peças"});return{User:e,setUser:s=>{e.value=s},unsetUser:()=>{e.value={}}}});export{u as m,c as u};
